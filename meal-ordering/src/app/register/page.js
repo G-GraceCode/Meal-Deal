6740ff3d0a974f0d34852dec53e0dd94f217f679
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-01-15 10:11:10 +0000

    Update route.js and page.js

--- meal-ordering/src/app/register/page.js
+++ meal-ordering/src/app/register/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
+import { useState } from "react";
 
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
@@ -16,8 +17,18 @@ export default function RegisterPage() {
         Register{" "}
       </h2>
       <form className="block max-w-sm mx-auto" onSubmit={handleFormSubmit}>
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
         <button type="submit">Register</button>
         <div className="my-4 text-center text-gray-500">
           or login with provider
