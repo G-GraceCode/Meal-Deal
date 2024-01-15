53dbec69a6502ebc432342467599b3ff2431ff2a
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-01-15 10:02:28 +0000

    Update 6 files

--- meal-ordering/src/app/register/page.js
+++ meal-ordering/src/app/register/page.js
@@ -1,14 +1,21 @@
+"use client";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
 
 export default function RegisterPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <section className="mx-3 mt-8">
       <h2 className="text-center text-primary text-4xl mb-8 font-bold">
         {" "}
         Register{" "}
       </h2>
-      <form className="block max-w-sm mx-auto">
+      <form className="block max-w-sm mx-auto" onSubmit={handleFormSubmit}>
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
         <button type="submit">Register</button>
