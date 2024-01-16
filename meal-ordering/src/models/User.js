a88c937210c29cf8f1d232f5716158dedc78fd9d
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-01-16 14:34:10 +0000

    Update User.js and tsconfig.json

--- meal-ordering/src/models/User.js
+++ meal-ordering/src/models/User.js
@@ -1,22 +1,25 @@
 import { models, Schema, model } from "mongoose";
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    require: true,
-    validate: () => {
-      if (password.length || password.length < 5) {
-        throw new Error("Pass Word Must be Greater than 5");
-        return false;
-      }
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      validate: () => {
+        if (!password?.length || password?.length < 5) {
+          throw new Error("Pass Word Must be Greater than 5");
+          return false;
+        }
+      },
     },
   },
-});
+  { timestamps: true },
+);
 
 const User = models?.User || model("User", userSchema);
 
