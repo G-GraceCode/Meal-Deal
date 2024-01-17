820ef46f0413c841294cdae30a422ff326e14f43
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-01-17 18:38:36 +0000

    Update Navbar.js and Sidenav.js

--- meal-ordering/src/components/navbar/Navbar.js
+++ meal-ordering/src/components/navbar/Navbar.js
@@ -2,8 +2,9 @@
 import Link from "next/link";
 import { useSession, signOut } from "next-auth/react";
 import { RiMenuUnfoldFill } from "react-icons/ri";
+import { IoIosNotifications } from "react-icons/io";
 
-export default function Navbar() {
+export default function Navbar({ close }) {
   const session = useSession();
   console.log("user", session);
   const status = session.status;
@@ -17,7 +18,7 @@ export default function Navbar() {
     <nav className="sticky top-0 inset-x-0 bg-[#fefecc]">
       <div className="flex flex-row items-center justify-between p-2">
         <div className="flex items-center gap-3">
-          <RiMenuUnfoldFill size={25} />
+          <RiMenuUnfoldFill size={25} onClick={close} />
           <Link
             href={"/"}
             className="font-semibold text-2xl no-underline text-secondary"
@@ -28,16 +29,19 @@ export default function Navbar() {
         <div>
           {status === "authenticated" && (
             <div className="flex items-center justify-center gap-3">
-              <Link
-                href={"/profile"}
-                className="no-underline flex items-center mr-2"
-                title="Your Profile"
-              >
-                <span className="w-8 h-8 mr-px rounded-full bg-primary flex items-center justify-center text-sm text-fontSecondary">
-                  {currentUser[0]}
-                </span>
-                {currentUser}
-              </Link>
+              <div className="flex items-center justify-center gap-2">
+                <IoIosNotifications size={25} />
+                <Link
+                  href={"/profile"}
+                  className="no-underline flex items-center mr-2"
+                  title="Your Profile"
+                >
+                  <span className="w-8 h-8 mr-px rounded-full bg-primary flex items-center justify-center text-sm text-fontSecondary">
+                    {currentUser[0]}
+                  </span>
+                  {currentUser}
+                </Link>
+              </div>
               <button
                 onClick={() => signOut()}
                 className="py-2 px-6 rounded-full text-xs border-none bg-primary text-fontSecondary mr-3 no-underline"
