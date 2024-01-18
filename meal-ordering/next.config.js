209858bd3c42743bb7c44df747ab34ae271b0ef8
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-01-18 09:07:00 +0000

    Update 3 files

--- meal-ordering/next.config.js
+++ meal-ordering/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+// this config, help us to add add photo from a different site, like google
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.googleusercontent.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
--- meal-ordering/src/app/profile/page.js
+++ meal-ordering/src/app/profile/page.js
@@ -1,7 +1,28 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { Image } from "next/image";
+import { redirect } from "next/navigation";
+
 export default function ProfilePage() {
+  const session = useSession();
+  const { status } = session;
+  const userImage = session.data?.user;
+  if (status === "Loading") {
+    return "Loading ...";
+  }
+
+  if (status === "unauthenticated") {
+    return redirect("/login");
+  }
+
   return (
-    <section>
-      <h1>profile</h1>
+    <section className="mb-8">
+      <h1 className="text-center text-primary text-4xl mb-4">profile</h1>
+      <form className="max-w-md mx-auto border">
+        <div>
+          <Image src={} width={65} height={65} alt={"avater"} />
+        </div>
+      </form>
     </section>
   );
 }
