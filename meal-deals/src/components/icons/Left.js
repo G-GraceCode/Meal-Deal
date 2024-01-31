5439268700274c1a2c8ca2b3bce60397f6293f1d
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-01-31 21:22:28 +0000

    New updates to the icons

--- meal-deals/src/components/icons/Left.js
+++ meal-deals/src/components/icons/Left.js
@@ -7,9 +7,9 @@ export default function Left({ className = "w-6 h-6" }) {
       className="w-6 h-6"
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
