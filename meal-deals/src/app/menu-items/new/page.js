a83b6539f9722420cfcb388649ed00951be1c0f1
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-02-05 14:58:12 +0000

    size, and ingredient form added

--- meal-deals/src/app/menu-items/new/page.js
+++ meal-deals/src/app/menu-items/new/page.js
@@ -4,15 +4,11 @@ import toast from "react-hot-toast";
 import { useState } from "react";
 import Usertabs from "@/components/layout/Usertabs";
 import ImageEdit from "@/components/layout/ImageEdit";
+import MenuItemForm from "@/components/layout/MenuItemForm";
 import { IoChevronBack } from "react-icons/io5";
 
 export default function NewMenuItemsPage() {
   const { loading, data } = userProfile();
-
-  const [image, setImage] = useState("");
-  const [name, setName] = useState("");
-  const [descript, setDescript] = useState("");
-  const [basePrice, setBasePrice] = useState("");
 
   const handleFormSumbit = async (e, data) => {
     e.preventDefault();
@@ -44,41 +40,10 @@ export default function NewMenuItemsPage() {
       <div className="text-left flex items-center justify-center gap-2">
         <IoChevronBack size={20} /> <span>Go Back to All Menus</span>
       </div>
-      <form className="mt-8 max-w-md mx-auto" onSubmit={handleFormSumbit}>
-        <div
-          className="grid items-start gap-4"
-          style={{ gridTemplateColumns: ".3fr .7fr" }}
-        >
-          <div>
-            <ImageEdit link={image} setLink={setImage} />
-          </div>
-
-          <div className="grow">
-            <label>Item Name</label>
-            <input
-              type="text"
-              placeholder="Item Name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label>Description</label>
-            <input
-              type="text"
-              placeholder="Description"
-              value={descript}
-              onChange={(e) => setDescript(e.target.value)}
-            />
-            <label>Price</label>
-            <input
-              type="text"
-              placeholder="Base Price"
-              value={basePrice}
-              onChange={(e) => setBasePrice(e.target.value)}
-            />
-            <button type="submit">Save</button>
-          </div>
-        </div>
-      </form>
+      <MenuItemForm
+        className="mt-8 max-w-md mx-auto"
+        onSubmit={handleFormSumbit}
+      />
     </section>
   );
 }
--- meal-deals/src/components/layout/MenuItemForm.js
+++ meal-deals/src/components/layout/MenuItemForm.js
@@ -1,17 +1,31 @@
 "use client";
-import { useState, useEffect } from "react";
 import ImageEdit from "@/components/layout/ImageEdit";
+import ItemsPriceProps from "@/components/layout/ItemsPriceProps";
+import { useState } from "react";
 
 export default function MenuItemForm({ handleForm, menuItems }) {
   const [image, setImage] = useState(menuItems?.image || "");
   const [name, setName] = useState(menuItems?.name || "");
   const [descript, setDescript] = useState(menuItems?.description || "");
   const [basePrice, setBasePrice] = useState(menuItems?.basePrice || "");
+  const [sizes, setSizes] = useState(menuItems?.sizes || []);
+  const [extraIngredients, setExtraIngredients] = useState(
+    menuItems?.extraIngredients || [],
+  );
 
   return (
     <form
       className="mt-8 max-w-md mx-auto"
-      onSubmit={(e) => handleForm(e, { image, name, descript, basePrice })}
+      onSubmit={(e) =>
+        handleForm(e, {
+          image,
+          name,
+          description: descript,
+          basePrice,
+          sizes,
+          extraIngredients,
+        })
+      }
     >
       <div
         className="grid items-start gap-4"
@@ -42,6 +56,19 @@ export default function MenuItemForm({ handleForm, menuItems }) {
             placeholder="Base Price"
             value={basePrice}
             onChange={(e) => setBasePrice(e.target.value)}
+          />
+
+          <ItemsPriceProps
+            name={"Sizes"}
+            addLabel={"Add item sizeitem size"}
+            props={sizes}
+            setProps={setSizes}
+          />
+          <ItemsPriceProps
+            name={"Extra ingredients"}
+            addLabel={"Add item ingredient prices"}
+            props={extraIngredients}
+            setProps={setExtraIngredients}
           />
           <button type="submit">Save</button>
         </div>
