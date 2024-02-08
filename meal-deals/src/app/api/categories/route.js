af78399f0662587e26c78af02d2ec3d101dd2d81
Tandu Yanmick Ateh <95031570+G-GraceCode@users.noreply.github.com>
2024-02-08 23:19:58 +0000

    changes made

--- meal-deals/src/app/api/categories/route.js
+++ meal-deals/src/app/api/categories/route.js
@@ -5,7 +5,7 @@ export async function POST(req) {
   mongoose.connect(process.env.DB_URI);
 
   const { name } = await req.json();
-  const categoryDoc = await Category.create(name);
+  const categoryDoc = await Category.create({ name });
   return Response.json(categoryDoc);
 }
 
